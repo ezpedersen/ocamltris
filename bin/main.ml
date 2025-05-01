0eabced9d368f9b137797b32d86cb4e7c9b99f0e
ezpedersen <erikzhangpedersen@gmail.com>
2025-05-01 16:48:03 -0400

    Implement ticking
    - Reorganize the main.ml into different loop functions
    - Game now does ticking but not at regular intervals
    - TODO: implement lwt sleeping

--- bin/main.ml
+++ bin/main.ml
@@ -10,6 +10,32 @@ let disable_raw_mode termios =
   let open Unix in
   tcsetattr stdin TCSANOW termios
 
+let render game =
+  for i = 0 to 50 do
+    print_newline ()
+  done;
+  for i = 0 to 19 do
+    for j = 0 to 9 do
+      print_string (if Tetris.get_entry game (j, i) then "#" else ".")
+    done;
+    print_newline ()
+  done
+
+let handle_input game =
+  let char = input_char stdin in
+  if char = 'q' then raise Exit;
+  if char = 'h' then Tetris.shift_right game (-1);
+  if char = 'l' then Tetris.shift_right game 1;
+  if char = 'a' then Tetris.rotate_ccw game;
+  if char = 'd' then Tetris.rotate_cw game;
+  if char = 'j' then Tetris.tick game
+
+let game_loop game =
+  Tetris.tick game;
+  handle_input game;
+  render game;
+  ()
+
 let () =
   let original_termios = Unix.tcgetattr Unix.stdin in
   enable_raw_mode ();
@@ -17,24 +43,7 @@ let () =
   let game = Tetris.create (10, 20) in
   try
     while true do
-      let char = input_char stdin in
-      if char = 'q' then raise Exit;
-      if char = 'h' then Tetris.shift_right game (-1);
-      if char = 'l' then Tetris.shift_right game 1;
-      if char = 'a' then Tetris.rotate_ccw game;
-      if char = 'd' then Tetris.rotate_cw game;
-      if char = 'j' then Tetris.tick game;
-      (* temp until I put a timer in *)
-      for i = 0 to 50 do
-        print_newline ()
-      done;
-      for i = 0 to 19 do
-        for j = 0 to 9 do
-          print_string (if Tetris.get_entry game (j, i) then "#" else ".")
-        done;
-        print_newline ()
-      done
-      (* yucky yucky hack front end*)
+      game_loop game
     done
   with Exit ->
     disable_raw_mode original_termios;
