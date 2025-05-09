7c3ad38c94b7812431961ab8f3b0958f0a8281f5
Major Zangari <maz76@cornell.edu>
2025-05-08 21:55:53 -0400

    2P garbage spawning

--- lib/tetris2P.ml
+++ lib/tetris2P.ml
@@ -26,18 +26,24 @@ let add_garbage g =
     g.right_garbaged_score <- right_score
   end
 
-let tick_left g = Tetris.tick g.left |> ignore
-let tick_right g = Tetris.tick g.right |> ignore
-let shift_left g n = Tetris.shift g.left n
-let shift_right g n = Tetris.shift g.right n
+let tick_left g =
+  Tetris.tick g.left |> ignore;
+  add_garbage g
+
+let tick_right g =
+  Tetris.tick g.right |> ignore;
+  add_garbage g
+
+let shift_left g n = Tetris.shift g.left n; add_garbage g
+let shift_right g n = Tetris.shift g.right n; add_garbage g
 let get_entry_left g (x, y) = Tetris.get_entry g.left (x, y)
 let get_entry_right g (x, y) = Tetris.get_entry g.right (x, y)
-let rotate_ccw_left g = Tetris.rotate_ccw g.left
-let rotate_ccw_right g = Tetris.rotate_ccw g.right
-let rotate_cw_left g = Tetris.rotate_cw g.left
-let rotate_cw_right g = Tetris.rotate_cw g.right
-let hard_drop_left g = Tetris.hard_drop g.left
-let hard_drop_right g = Tetris.hard_drop g.right
+let rotate_ccw_left g = Tetris.rotate_ccw g.left; add_garbage g
+let rotate_ccw_right g = Tetris.rotate_ccw g.right; add_garbage g
+let rotate_cw_left g = Tetris.rotate_cw g.left; add_garbage g
+let rotate_cw_right g = Tetris.rotate_cw g.right; add_garbage g
+let hard_drop_left g = Tetris.hard_drop g.left; add_garbage g
+let hard_drop_right g = Tetris.hard_drop g.right; add_garbage g
 let get_scores g = (Tetris.get_score g.left, Tetris.get_score g.right)
 let get_held g = (Tetris.get_held g.left, Tetris.get_held g.right)
 let hold_left g = Tetris.hold g.left
