ab3dd1bf7de68569f7ce9c21861fff00435c9d38
ezpedersen <erikzhangpedersen@gmail.com>
2025-05-15 12:47:51 -0700

    Add landing page
    - To clean up GUI
    - Using bogue for displaying clickable stuff
    - Showing controls only
    - WIP: start single player, 2 player and bots screen

---
+++ bin/main.ml
@@ -0,0 +1,95 @@
+open Bogue
+open Tsdl
+module W = Widget
+module L = Layout
+module T = Trigger
+
+let background_image = "static/bg.png"
+let text size str = W.label ~size ~fg:Draw.(opaque black) str
+
+let controls_window () =
+  let title = W.label ~size:32 "CONTROLS" in
+  let title_layout = L.resident title in
+  L.sety title_layout 30;
+
+  let section_title s = text 20 s |> L.resident in
+  let control_text s = text 18 s |> L.resident in
+
+  let left_col =
+    L.tower
+      [
+        section_title "Player 1:";
+        control_text "A/D: Move Left/Right";
+        control_text "W/S: Rotate CW/CCW";
+        control_text "Z: Soft Drop";
+        control_text "X: Hard Drop";
+        control_text "C: Hold";
+      ]
+  in
+
+  let right_col =
+    L.tower
+      [
+        section_title "Player 2:";
+        control_text "J/L: Move Left/Right";
+        control_text "I/K: Rotate CW/CCW";
+        control_text "M: Soft Drop";
+        control_text ". (period): Hard Drop";
+        control_text ", (comma): Hold";
+      ]
+  in
+
+  let general_controls =
+    L.tower [ control_text "P: Pause"; control_text "Q: Quit" ]
+  in
+
+  let all_controls =
+    L.tower ~sep:40
+      [
+        title_layout;
+        L.flat ~align:Draw.Center ~sep:100 [ left_col; right_col ];
+        general_controls;
+      ]
+  in
+
+  let win = Window.create all_controls in
+  let board = Main.create [ win ] in
+  Main.run board
+
+let open_new_window title content =
+  let label = W.label content in
+  let layout = L.resident label in
+  let win = Window.create layout in
+  let board = Main.create [ win ] in
+  Main.run board
+
+let make_button label callback =
+  let b = W.button label in
+  W.on_click b ~click:(fun _ -> callback ());
+  b
+
+let main () =
+  let bg = W.image ~w:800 ~h:600 background_image in
+  let bg_room = L.resident bg in
+
+  let single =
+    make_button "Single Player" (fun () ->
+        open_new_window "Single Player" "TODO")
+  in
+  let multi =
+    make_button "2 Player" (fun () -> open_new_window "2 Player" "TODO")
+  in
+  let bots = make_button "Bots" (fun () -> open_new_window "Bots" "TODO") in
+  let controls = make_button "Controls" (fun () -> controls_window ()) in
+
+  let buttons =
+    L.flat ~align:Draw.Center ~sep:40
+      (List.map L.resident [ single; multi; bots; controls ])
+  in
+  L.sety buttons 500;
+
+  let layout = L.superpose [ bg_room; buttons ] in
+  let board = Main.of_layout layout in
+  Main.run board
+
+let () = main ()
